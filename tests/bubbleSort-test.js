51013d6be9a596b608c3fc944ed5f61a5caa7666
James Logue <logueja@gmail.com>
2017-12-13 11:18:15 -0700

    Update testing, refactor quickSort to be faster

--- tests/bubbleSort-test.js
+++ tests/bubbleSort-test.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai').use(require("chai-sorted"))
 const bubbleSort = require('../lib/bubbleSort.js');
+const { randomNumberGen } = require('@jjlljj/random-number-gen')
 
 describe('bubbleSort', () => {
   it('should be a function', () => {
@@ -11,9 +12,27 @@ describe('bubbleSort', () => {
     expect(bubbleSort(array)).to.deep.equal([1, 2, 3, 4, 5])
   })
 
-  it('should sort arrays with negative numbers', ()=> {
+  it('should sort an array with negative numbers', ()=> {
     const array = [3, 2, -5, -1, 4]
     expect(bubbleSort(array)).to.deep.equal([-5, -1, 2, 3, 4])
+  })
+
+  it('should sort an array of 1,001 numbers', ()=> {
+    const array = randomNumberGen(1001, 2500)
+    expect(array.length).to.equal(1001)
+    expect(bubbleSort(array)).to.be.sorted()
+  })
+
+  it('should sort an array of 5,000 numbers', ()=> {
+    const array = randomNumberGen(5000, 7500)
+    expect(array.length).to.equal(5000)
+    expect(bubbleSort(array)).to.be.sorted()
+  })
+
+   it('should sort an array of 7,500 numbers', ()=> {
+    const array = randomNumberGen(7000, 10000)
+    expect(array.length).to.equal(7000)
+    expect(bubbleSort(array)).to.be.sorted()
   })
 
 })
